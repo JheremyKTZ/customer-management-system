51b25b6dae928afcd4bbdfcc4f96d83eab433791
Elton Cassas <elcassastn50@gmail.com>
2024-01-10 13:42:24 -0400

    update scripts

--- LINQ.Playground/Scripts/CleanTables.sql
+++ LINQ.Playground/Scripts/CleanTables.sql
@@ -1,95 +1,17 @@
 ﻿BEGIN
-	PRINT('Creating Products Table');
-		CREATE TABLE [dbo].[Products](
-			[Id] [int] NOT NULL,
-			[Name] [varchar](200) NOT NULL,
-			[Description] [varchar](300) NOT NULL,
-			[CurrentPrice] [decimal](16, 2) NULL,
-		 CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED 
-		(
-			[Id] ASC
-		)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-		) ON [PRIMARY];
+	PRINT('Removing data from Products Table');
+	DELETE FROM [dbo].[Products]
 
-	PRINT('Creating Customers Table');
-	CREATE TABLE [dbo].[Customers](
-		[Id] [int] NOT NULL,
-		[Type] [int] NOT NULL,
-		[FirstName] [nvarchar](100) NULL,
-		[LastName] [nvarchar](100) NULL,
-		[Email] [nvarchar](200) NULL,
-	 CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED 
-	(
-		[Id] ASC
-	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-	) ON [PRIMARY];
+	PRINT('Removing data from Customers Table');
+	DELETE FROM [dbo].[Customers]
 
-	PRINT('Creating Addresses Table');
-	CREATE TABLE [dbo].[Addresses](
-		[Id] [int] NOT NULL,
-		[AddressLine1] [nvarchar](100) NOT NULL,
-		[AddressLine2] [nvarchar](100) NULL,
-		[City] [varchar](100) NOT NULL,
-		[State] [varchar](50) NOT NULL,
-		[PostalCode] [varchar](50) NOT NULL,
-		[Country] [varchar](50) NOT NULL,
-		[Type] [int] NOT NULL,
-		[CustomerId] [int] NOT NULL,
-	 CONSTRAINT [PK_Addresses] PRIMARY KEY CLUSTERED 
-	(
-		[Id] ASC
-	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-	) ON [PRIMARY];
+	PRINT('Removing data from Addresses Table');
+	DELETE FROM [dbo].[Addresses]
 
-	ALTER TABLE [dbo].[Addresses]  WITH NOCHECK ADD  CONSTRAINT [FK_Addresses_ToCustomers] FOREIGN KEY([CustomerId])
-	REFERENCES [dbo].[Customers] ([Id])
-	ALTER TABLE [dbo].[OrderItems] CHECK CONSTRAINT [FK_OrderItems_ToOrders]
+	PRINT('Removing data from Orders Table');
+	DELETE FROM [dbo].[Orders]
 
-	PRINT('Creating [Orders] Table');
-	CREATE TABLE [dbo].[Orders](
-	[Id] [int] NOT NULL,
-	[CustomerId] [int] NOT NULL,
-	[Date] [datetime] NULL,
-	[ShippingAddressId] [int] NOT NULL,
-	 CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED 
-	(
-		[Id] ASC
-	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-	) ON [PRIMARY]
-	ALTER TABLE [dbo].[Orders]  WITH NOCHECK ADD  CONSTRAINT [FK_Orders_ToAddresses] FOREIGN KEY([ShippingAddressId])
-	REFERENCES [dbo].[Addresses] ([Id]);
-	ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_ToAddresses];
-	ALTER TABLE [dbo].[Orders]  WITH NOCHECK ADD  CONSTRAINT [FK_Orders_ToCustomers] FOREIGN KEY([CustomerId])
-	REFERENCES [dbo].[Customers] ([Id]);
-	ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_ToCustomers];
-
-	PRINT('Creating OrderItems Table');
-	CREATE TABLE [dbo].[OrderItems](
-		[Id] [int] NOT NULL,
-		[ProductId] [int] NOT NULL,
-		[PurchasePrice] [decimal](16, 2) NOT NULL,
-		[Quantity] [int] NOT NULL,
-		[OrderId] [int] NOT NULL,
-	 CONSTRAINT [PK_OrderItems] PRIMARY KEY CLUSTERED 
-	(
-		[Id] ASC
-	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-	) ON [PRIMARY];
-
-	ALTER TABLE [dbo].[OrderItems]  WITH NOCHECK ADD  CONSTRAINT [FK_OrderItems_ToOrders] FOREIGN KEY([OrderId])
-	REFERENCES [dbo].[Orders] ([Id])
-	ALTER TABLE [dbo].[OrderItems] CHECK CONSTRAINT [FK_OrderItems_ToOrders]
-	ALTER TABLE [dbo].[OrderItems]  WITH NOCHECK ADD  CONSTRAINT [FK_OrderItems_ToProducts] FOREIGN KEY([ProductId])
-	REFERENCES [dbo].[Products] ([Id])
-	ALTER TABLE [dbo].[OrderItems] CHECK CONSTRAINT [FK_OrderItems_ToProducts]
-
-
-
-	---- Remove Information from Tables
-	--DELETE * FROM [dbo].[Products]
-	--DELETE * FROM [dbo].[Customers]
-	--DELETE * FROM [dbo].[Addresses]
-	--DELETE * FROM [dbo].[Orders]
-	--DELETE * FROM [dbo].[OrderItems]
+	PRINT('Removing data from OrderItems Table');
+	DELETE FROM [dbo].[OrderItems]
 END
 GO
--- LINQ.Playground/Scripts/CreateTables.sql
+++ LINQ.Playground/Scripts/CreateTables.sql
@@ -1,4 +1,26 @@
 ﻿BEGIN
+    PRINT('Creating Products Table');
+    CREATE TABLE [dbo].[Products] (
+        [Id]           INT             NOT NULL,
+        [Name]         VARCHAR (200)   NOT NULL,
+        [Description]  VARCHAR (300)   NOT NULL,
+        [CurrentPrice] DECIMAL (16, 2) NULL,
+        CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([Id] ASC) 
+            WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+    ) ON [PRIMARY];
+
+    PRINT('Creating Customers Table');
+    CREATE TABLE [dbo].[Customers] (
+        [Id]        INT            NOT NULL,
+        [Type]      INT            NOT NULL,
+        [FirstName] NVARCHAR (100) NULL,
+        [LastName]  NVARCHAR (100) NULL,
+        [Email]     NVARCHAR (200) NULL,
+        CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED ([Id] ASC) 
+            WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	) ON [PRIMARY];
+
+    PRINT('Creating Addresses Table');
     CREATE TABLE [dbo].[Addresses] (
         [Id]           INT            NOT NULL,
         [AddressLine1] NVARCHAR (100) NOT NULL,
@@ -8,34 +30,28 @@
         [PostalCode]   VARCHAR (50)   NOT NULL,
         [Country]      VARCHAR (50)   NOT NULL,
         [Type]         INT            NOT NULL,
+        [CustomerId]   INT            NOT NULL,
         CONSTRAINT [PK_Addresses] PRIMARY KEY CLUSTERED ([Id] ASC)
-    );
+            WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	) ON [PRIMARY];
 
-    CREATE TABLE [dbo].[Products] (
-        [Id]           INT             NOT NULL,
-        [Name]         VARCHAR (200)   NOT NULL,
-        [Description]  VARCHAR (300)   NOT NULL,
-        [CurrentPrice] DECIMAL (16, 2) NULL,
-        CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([Id] ASC)
-    );
-
-    CREATE TABLE [dbo].[Customers] (
-        [Id]        INT            NOT NULL,
-        [Type]      INT            NOT NULL,
-        [FirstName] NVARCHAR (100) NULL,
-        [LastName]  NVARCHAR (100) NULL,
-        [Email]     NVARCHAR (200) NULL,
-        CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED ([Id] ASC)
-    );
+    ALTER TABLE [dbo].[Addresses]  WITH NOCHECK ADD  CONSTRAINT [FK_Addresses_ToCustomers] FOREIGN KEY([CustomerId])
+	REFERENCES [dbo].[Customers] ([Id])
+    
+    PRINT('Creating [Orders] Table');
     CREATE TABLE [dbo].[Orders] (
         [Id]                INT      NOT NULL,
         [CustomerId]        INT      NOT NULL,
         [Date]              DATETIME NULL,
         [ShippingAddressId] INT      NOT NULL,
-        CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED ([Id] ASC),
+        CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED ([Id] ASC) 
+            WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
         CONSTRAINT [FK_Orders_ToCustomers] FOREIGN KEY ([CustomerId]) REFERENCES [Customers]([Id]), 
         CONSTRAINT [FK_Orders_ToAddresses] FOREIGN KEY ([ShippingAddressId]) REFERENCES [Addresses]([Id])
     );
+
+
+    PRINT('Creating OrderItems Table');
     CREATE TABLE [dbo].[OrderItems]
     (
 	    [Id] INT NOT NULL , 
@@ -43,7 +59,8 @@
         [PurchasePrice] DECIMAL(16, 2) NOT NULL, 
         [Quantity] INT NOT NULL, 
         [OrderId] INT NOT NULL,
-	    CONSTRAINT [PK_OrderItems] PRIMARY KEY CLUSTERED ([Id] ASC),
+	    CONSTRAINT [PK_OrderItems] PRIMARY KEY CLUSTERED ([Id] ASC) 
+            WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
         CONSTRAINT [FK_OrderItems_ToOrders] FOREIGN KEY ([OrderId]) REFERENCES [Orders]([Id]), 
         CONSTRAINT [FK_OrderItems_ToProducts] FOREIGN KEY ([ProductId]) REFERENCES [Products]([Id])
     );
